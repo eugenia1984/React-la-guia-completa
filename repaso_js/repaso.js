787153e7a6c9435a6e78e221d9cd30bba7b918a7
Eugenia <costamariaeugenia1@gmail.com>
2022-12-18 17:47:18 -0300

    Update repaso.js

---
+++ repaso_js/repaso.js
@@ -0,0 +1,6 @@
+/****** Repaso de JavaScript desde ES6+ *****/
+
+// variables
+// inicialmente se utilizaba var, pero por hoisting es mejor usar let o const
+let nombre = "Eugenia"; // declaro y asigno el valor 
+console.log(`Hola ${nombre}`); // la muestro por consola
