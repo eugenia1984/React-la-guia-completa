e32a5cfa2b71ceaf3c40b8c343991e48efd42f1d
Eugenia <costamariaeugenia1@gmail.com>
2022-12-19 16:18:41 -0300

    Update modules.js

--- repaso_js/modules.js
+++ repaso_js/modules.js
@@ -1,1 +1,17 @@
 /*** Modulos en JavaScript ***/
+// si este archivo esta cargandose en un HTML, agrego: type="module"
+// <script src="modules.js" type="module"></script>
+
+// importo una variable y dos funciones
+import { nameTask, createTask, completedTask } from "./clases.js";
+
+// si seria exportado por default
+// import nameTask from "./clases.js";
+
+// utilizo la variable que importe
+console.log(nameTask);
+
+// utilizo la funcion que importe
+const tarea1 = createTask("Eat ice-cream", "low");
+console.log(tarea1);
+completedTask(); 
