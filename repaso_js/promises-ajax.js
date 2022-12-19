f5984feaa96a0fbe3a1d294a368426b9b77d3095
Eugenia <costamariaeugenia1@gmail.com>
2022-12-19 14:49:17 -0300

    Create promises-ajax.js

---
+++ repaso_js/promises-ajax.js
@@ -0,0 +1,39 @@
+/*** Promises with ajax ****/
+
+const downloadUsers = amount => new Promise( (res, rej) => {
+  // pasar a cantidad a la api
+  const api = `https://randomuser.me/api/?results=${amount}&nat=us`;
+  
+  //call to ajax
+  const xhr = new XMLLHttpRequest();
+  
+  // open connection with ajax
+  // 1er param: GET | POST
+  // 2do param: la URL
+  // 3er param: si es asincrono true, si no es asincrono false
+  xhr.open("GET", api, true);
+  
+  // on load read the response and send it
+  xhr.onload = () => {
+    if(xhr.status === 200) {
+      // JSON.parse para pasarlo de String a object 
+      res(JSON.parse(xhr.responseText, results))
+    } else {
+      rej(Error(xhr.statusText))
+    }
+  }
+  
+  // on error (it´s optional)
+  xhr.oneror = (error) => rej(error);
+  
+  // send
+  xhr.send();
+});
+
+downloadUsers(20).
+  then(
+    members => console.log(members);
+    error => consol.error(
+      new Error("Hubo un error" + error);
+    ) 
+  );
