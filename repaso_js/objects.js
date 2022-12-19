b99c1536abfbeac6842cf7d4b80e2939a4b6e21f
Eugenia <costamariaeugenia1@gmail.com>
2022-12-19 11:36:23 -0300

    agrego el archivo de objetos

---
+++ repaso_js/objects.js
@@ -0,0 +1,18 @@
+/**** Objects ****/
+// Object Literal
+// Se declara entre {}, tiene key:value
+const persona = {
+  nombre: "Juan",
+  profesion: "Desarrollador web",
+  edad: 40,
+  isWorking: true
+}
+
+// muestro todo el objeto
+console.log(persona);
+
+// accedo a un valor con dot notation
+console.log(persona.nombre); // Juan
+
+// accedo a un valor con los []
+console.log(persona["edad"]); // 40
