908524ce58670f3bc8715cd1365ee7f1a2243122
Eugenia <costamariaeugenia1@gmail.com>
2022-12-19 12:38:02 -0300

    Funciones en un objeto

--- repaso_js/object-function.js
+++ repaso_js/object-function.js
@@ -1,1 +1,18 @@
 /**** Funciones en un objeto ****/
+// Con ES6+
+const person = {
+  fullName: "Maria Eugenia Costa",
+  work: "Front End Developer",
+  age: 38,
+  musicPop: true,
+  showInfo() {
+    console.log(`${this.fullName} is ${this.work} and has ${this.age}`)
+  }
+}
+
+console.log(showInfo);
+
+// Con ES5
+// showInfo: function() {
+//  console.log(`${this.fullName} is ${this.work} and has ${this.age}`)
+// }
